2cf3cd1444e011eb805c6f4ca32a8a08710f577b
Adam Harvey <adaharve@cisco.com>
2022-03-09 10:19:47 -0500

    initial commit

---
+++ apps/dependabot-targz/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dependabot-targz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'dependabot-targz';
+}
